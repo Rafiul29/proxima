0cf2456d35cfbd3e966f8ef9a6b7754c47109645
Rafiul29 <rafiulislam4472@gmail.com>
2023-02-20 18:45:34 +0600

    cors added

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const cors=require('cors')
 const mongoose = require("mongoose");
 const projectRoutes = require("./routes/projectRoutes");
+
 const app = express();
 
 //port
 const port = process.env.PORT || 4000;
 
 //midlewares
+
 app.use(express.json());
+app.use(cors())
 app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
