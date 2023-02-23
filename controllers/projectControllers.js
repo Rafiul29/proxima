6500365a0ca74db06cb9c89606097d66fe61f960
Rafiul29 <rafiulislam4472@gmail.com>
2023-02-23 14:33:16 +0600

    error handle

--- controllers/projectControllers.js
+++ controllers/projectControllers.js
@@ -1,77 +1,100 @@
-const mongoose=require('mongoose')
-const Project = require('../models/projectModel');
+const mongoose = require("mongoose");
+const Project = require("../models/projectModel");
 
 //get all projects
-const getAllProject=async(req,res)=>{
-    const projects=await Project.find({}).sort({createdAt:-1})
-        res.status(200).json(projects)
-}
+const getAllProject = async (req, res) => {
+  const projects = await Project.find({}).sort({ createdAt: -1 });
+  res.status(200).json(projects);
+};
 
 //get single project
-const getSingleProject=async(req,res)=>{
+const getSingleProject = async (req, res) => {
+  const { id } = req.params;
 
-   const {id} =req.params;
-
-   if(!mongoose.Types.ObjectId.isValid(id)){
-    return res.status(404).json({error: "invalid id"})
-   }
-    const project =await Project.findById(id)
-    if(!project){
-        return res.status(404).json({error: "No project found"})
-    }
-    res.status(200).json(project)
-}
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "invalid id" });
+  }
+  const project = await Project.findById(id);
+  if (!project) {
+    return res.status(404).json({ error: "No project found" });
+  }
+  res.status(200).json(project);
+};
 
 //post a new project
-const postProject=async(req,res)=>{
-        const data=req.body;
-        try{
-        const project= await Project.create({
-                ...data,
-            })
-            res.status(200).json(project)
-        }catch(error){
-              res.status(400).json({error: error.message})
-        }
- }
+const postProject = async (req, res) => {
+  const { title, tech, budget, duration, manager, dev } = req.body;
+
+  let emptyFields = [];
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!tech) {
+    emptyFields.push("tech");
+  }
+  if (!budget) {
+    emptyFields.push("budget");
+  }
+  if (!duration) {
+    emptyFields.push("duration");
+  }
+  if (!manager) {
+    emptyFields.push("manager");
+  }
+  if (!dev) {
+    emptyFields.push("dev");
+  }
+
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "please fill in all fields", emptyFields });
+  }
+
+  try {
+    const project = await Project.create({
+      ...req.body,
+    });
+    res.status(200).json(project);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 // delete a project
-const deleteProject=async(req,res)=>{
-        const {id}=req.params
-        if(!mongoose.Types.ObjectId.isValid(id)){
-            return res.status(404).json({error: "Invalid Id"})
-        }
+const deleteProject = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid Id" });
+  }
 
-        const project=await Project.findOneAndDelete({_id:id});
+  const project = await Project.findOneAndDelete({ _id: id });
 
-        if(!project){
-            return res.status(400).json({error:"No project Found"})
-        }
+  if (!project) {
+    return res.status(400).json({ error: "No project Found" });
+  }
 
-        res.status(200).json(project)
-}
+  res.status(200).json(project);
+};
 
 //update a project
-const updateProject=async(req,res)=>{
-    const {id}=req.params
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: "Invalid Id"})
-    }
-    const project=await Project.findOneAndUpdate({_id:id},{...req.body});
+const updateProject = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid Id" });
+  }
+  const project = await Project.findOneAndUpdate({ _id: id }, { ...req.body });
 
-    if(!project){
-        return res.status(400).json({error:"No project Found"})
-    }
-    res.status(200).json(project)
-}
+  if (!project) {
+    return res.status(400).json({ error: "No project Found" });
+  }
+  res.status(200).json(project);
+};
 
-
-
-
- module.exports={
-    postProject,
-    getAllProject,
-    getSingleProject,
-    deleteProject,
-    updateProject
- }
+module.exports = {
+  postProject,
+  getAllProject,
+  getSingleProject,
+  deleteProject,
+  updateProject,
+};
