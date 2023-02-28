19d3ca7031f23f4990803df8a124413acffb5884
Rafiul29 <rafiulislam4472@gmail.com>
2023-02-28 21:05:41 +0600

    validator added

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const bcrypt=require("bcrypt")
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -15,21 +16,35 @@ const userSchema = new Schema({
 });
 
 userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  //check if the email is valid
+  if (!validator.isEmail(email)) {
+    throw Error("Invalid email");
+  }
+
+  //lowercase .uppercase,number symbol, 8 char
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error(
+      "Password is not strong, try to combine uppercase ,lowercase, number ,symbol, and minium 8 chars"
+    );
+  }
   const exist = await this.findOne({ email });
   if (exist) {
     throw Error("email already used");
   }
   //encrypt password or hasing
-  const salt=await bcrypt.genSalt(10);
-  const hash=await bcrypt.hash(password,salt)
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
 
   //create on user
 
-  const user=await this.create({email,password:hash})
+  const user = await this.create({ email, password: hash });
 
-  return user
-
+  return user;
 };
-
 
 module.exports = mongoose.model("User", userSchema);
